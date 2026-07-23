23145ea14f475b26d7ac8848c2ad2dcc0ca60a9f
frodelindtner <frodelindtner@hotmail.com>
2026-07-23 07:33:00 +0200

    removed PgAdmin

--- PlacementSolution.AppHost/AppHost.cs
+++ PlacementSolution.AppHost/AppHost.cs
@@ -1,7 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
-    .WithPgAdmin(pgAdmin => pgAdmin.WithHostPort(5050))
     .WithDataVolume();
 
 var standingsdb = postgres.AddDatabase("standingsdb");
